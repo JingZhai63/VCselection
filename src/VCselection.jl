930a161e746c20dd79d2f7c17cfdd3c015d2ea5c
Jing Zhai <jingzhai.email.arizona.edu>
2017-11-15 17:51:00 -0700

    updage module

--- src/VCselection.jl
+++ src/VCselection.jl
@@ -12,15 +12,15 @@ using StatsBase
     include("microVClasso.jl")
     include("VClasso_v5.jl")
   elseif VERSION < v"0.5-" && VERSION >= v"0.4-"
-    export
-    microVClasso,
-#    Polynomial,
-    VClasso
-
-    include("microVClasso.jl")
-    include("VClasso.jl")
     include("Polynomial.jl")
     using Polynomial
+
+    export
+    microVClasso,
+    VClasso
+#    Polynomial,
+    include("microVClasso.jl")
+    include("VClasso.jl")
 
   end
 
